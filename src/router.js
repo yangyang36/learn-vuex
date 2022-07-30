92a7ab257c8e82a6e40d7fc253af799baf0d933d
yangyang36 <9723006@nkust.edut.tw>
2022-07-30 18:08:33 +0800

    add header

--- src/router.js
+++ src/router.js
@@ -1,17 +1,28 @@
-import {createRouter, createWebHistory} from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachDetail from './pages/coaches/CoachDetail.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+import RequestReceived from './pages/requests/RequestReceived.vue';
+import NotFound from './pages/NotFound.vue';
 
 const router = createRouter({
-  history:createWebHistory(),
+  history: createWebHistory(),
   routes: [
-    {path:'/', redirect:'/coaches'}
-    {path:'/coaches' , component:null},
-    {path:'/coaches/:id', component: null , children:[
-      {path:'/contact', component:null} //coaches/id/contact
-    ]},
-    {path:'/register', component: null},
-    {path:'/requests', component:null},
-    {path:'/:notFound(.*)', component:null}
-  ]
-})
+    { path: '/', redirect: '/coaches' },
+    { path: '/coaches', component: CoachesList },
+    {
+      path: '/coaches/:id',
+      component: CoachDetail,
+      children: [
+        { path: 'contact', component: ContactCoach }, //coaches/id/contact
+      ],
+    },
+    { path: '/register', component: CoachRegistration },
+    { path: '/requests', component: RequestReceived },
+    { path: '/:notFound(.*)', component: NotFound },
+  ],
+});
 
-export default router
+export default router;
